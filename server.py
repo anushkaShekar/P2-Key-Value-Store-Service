385f07f9d9ff8c3541c5dfa508e69ab736ec0b55
Anushka Chandrashekar <anushka@cs544.c.project-2-308905.internal>
2023-02-25 16:58:28 +0000

    Second Commit

--- server.py
+++ server.py
@@ -1,15 +1,23 @@
 import grpc
 from concurrent import futures
 import numstore_pb2_grpc
+import numstore_pb2
+
+values = {}
+total = 0
 
 class NumStore(numstore_pb2_grpc.NumStoreServicer):
-    def Temp(self, request, context):
+    def SetNum(self, request, context):
+        if request.key not in values:
+            values[request.key] = request.value
+            total = total + request.value
+        return numstore_pb2.SetNumResp(total=total)
 
 server = grpc.server(futures.ThreadPoolExecutor(max_workers=4), options=[("grpc.so_reuseport", 0)])
-# TODO: add servicer
 
+numstore_pb2_grpc.add_NumStoreServicer_to_server(NumStore(), server)
 
-server.add_insecure_port('localhost:5444')
+server.add_insecure_port('localhost:5440')
 server.start()
 server.wait_for_termination()
 
