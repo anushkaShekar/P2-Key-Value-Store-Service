d725f77143e40c0a8a3260c79d99ba17aa988673
Anushka Chandrashekar <anushkashikar@gmail.com>
2023-02-28 20:49:17 +0000

    Fourth Commit

--- client.py
+++ client.py
@@ -1,5 +1,7 @@
 import sys
 import grpc
+import threading
+import random, string
 import numstore_pb2_grpc
 import numstore_pb2
 
@@ -8,16 +10,51 @@ addr = f"127.0.0.1:{port}"
 channel = grpc.insecure_channel(addr)
 stub = numstore_pb2_grpc.NumStoreStub(channel)
 
+def task():
+    key_list = []
+    for i in range(100):
+        val = random.choice(string.ascii_uppercase)
+        key_list.append(val)
+
+    #for i in range(8):
+        #create_thread()
+        #for i in range(100):
+            #t.start()
+            #t.join()
+
+    return key_list
+
+def create_thread():
+    t = threading.Thread(target=thread_function)
+
+def thread_function():
+    keys = task()
+    randomKey = random.choice(keys)
+    randomValue = random.randint(1, 15)
+    function_list = [stub.SetNum(numstore_pb2.SetNumRequest()), stub.Fact(numstore_pb2.FactRequest())]
+    randomFunction = random.choice(function_list)
+
+    if randomFunction == stub.SetNum(numstore_pb2.SetNumRequest()):
+        resp = randomFunction(key=randomKey, value=randomValue)
+        print(resp.total)
+    else:
+        resp = randomFunction(key=randomKey)
+        print(resp.value)
+
+task()
+thread_function()
+
 # TEST SetNum
-resp = stub.SetNum(numstore_pb2.SetNumRequest(key="A", value=1))
-print(resp.total) # should be 1
-resp = stub.SetNum(numstore_pb2.SetNumRequest(key="B", value=10))
-print(resp.total) # should be 11
-resp = stub.SetNum(numstore_pb2.SetNumRequest(key="A", value=5))
-print(resp.total) # should be 15
-resp = stub.SetNum(numstore_pb2.SetNumRequest(key="B", value=0))
-print(resp.total) # should be 5
+#resp = stub.SetNum(numstore_pb2.SetNumRequest(key="A", value=1))
+#print(resp.total) # should be 1
+#resp = stub.SetNum(numstore_pb2.SetNumRequest(key="B", value=10))
+#print(resp.total) # should be 11
+#resp = stub.SetNum(numstore_pb2.SetNumRequest(key="A", value=5))
+#print(resp.total) # should be 15
+#resp = stub.SetNum(numstore_pb2.SetNumRequest(key="B", value=0))
+#print(resp.total) # should be 5
 
 # TEST Fact
-resp = stub.Fact(numstore_pb2.FactRequest(key="A"))
-print(resp.value) # should be 120
+#resp = stub.Fact(numstore_pb2.FactRequest(key="A"))
+#print(resp.value) # should be 120
+#print(resp.hit) # should be false
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import grpc
+import threading
 from concurrent import futures
 import numstore_pb2_grpc
 import numstore_pb2
@@ -7,29 +8,47 @@ values = {}
 total = 0
 factorial = 1
 i = 1
+cache_size = 10
+cache = {} # key is the value corresponding to the key request, value is the factorial of that value
+#hit = False
+lock = threading.Lock()
 
 class NumStore(numstore_pb2_grpc.NumStoreServicer):
     def SetNum(self, request, context):
-        if request.key not in values:
-            values[request.key] = request.value
-            global total
-            total = total + request.value
-        elif request.value != values[request.key]:
-            total = total - values[request.key]
-            total = total + request.value
-            values[request.key] = request.value
+        with lock:
+            if request.key not in values:
+                values[request.key] = request.value
+                global total
+                total = total + request.value
+            elif request.value != values[request.key]:
+                total = total - values[request.key]
+                total = total + request.value
+                values[request.key] = request.value
         return numstore_pb2.SetNumResp(total=total)
 
     def Fact(self, request, context):
+        val = values[request.key]
         if request.key in values:
-            global i
-            while i <= values[request.key]:
-                global factorial
-                factorial = factorial * i
-                i = i + 1
+            # Eviction policy: LRU
+            if val in cache:
+                #global hit
+                hit = True
+                fact = cache[val]
+                cache.pop(val)
+                cache[val] = fact
+            else:
+                hit = False
+                global i
+                while i <= val:
+                    global factorial
+                    factorial = factorial * i
+                    i += 1
+                cache[val] = factorial
+                if len(cache) > cache_size:
+                    cache.pop(0)
         else:
-            print("Key can not be found")
-        return numstore_pb2.FactResp(value=factorial)
+            print("key does not exist")
+        return numstore_pb2.FactResp(value=cache[val], hit=hit)
 
 server = grpc.server(futures.ThreadPoolExecutor(max_workers=4), options=[("grpc.so_reuseport", 0)])
 
